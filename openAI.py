e91798d4fdb4210558df697ded0e6fcb53f70f39
DanielWang2002 <sf61224@gmail.com>
2023-01-24 17:44:54 +0800

    fix: 將openai_apikey改成json讀入

--- openAI.py
+++ openAI.py
@@ -1,8 +1,27 @@
 import openai
-openai.api_key = "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"
+import json
+
+f = open('./openai_key.json')
+data = json.load(f)
+
+openai.api_key = data['key']
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt="\nHuman:用java演示dfs",
+  temperature=0.9,
+  max_tokens=2048,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
+print(response["choices"][0]["text"])
+
+print("====================")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="\nHuman:用java演示bfs",
   temperature=0.9,
   max_tokens=2048,
   top_p=1,
